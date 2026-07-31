2dd3c4fe673d0d15d16c7d6c5d0ffc1720e6cfe3
Lovepreet Singh <lovepreetsingh200626@gmail.com>
2026-07-31 18:23:49 +0530

    cors error solved

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Middleware: CORS Configured for Vite React Dev Server
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [process.env.FRONTEND_URL, 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
